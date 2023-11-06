457435a713fe1a04b9f645d5792f742dc48dd471
mayoush89k <43721924+mayoush89k@users.noreply.github.com>
2023-11-06 22:00:08 +0200

    my work in one page

---
+++ assets/script.js
@@ -0,0 +1,222 @@
+// import worldWikipediaPic from './images/worldWikipedia.jpg'
+
+const myWork = {
+  myGamesList: [
+    {
+      id: 1,
+      name: "Snake Game",
+      src: "./images/snake.jpg",
+      alt: "snake game photo",
+      netlifyLink: "https://snake-game-mk-js.netlify.app/",
+      githubLink: "https://github.com/mayoush89k/snakeGame",
+      description:
+        "This is a game that snake is moving toward food (the food built by random position) but the snake move by users arrow keys, the game finished when the snake hit itself or hit the border of the game",
+    },
+    {
+      id: 2,
+      name: "Memory Game",
+      src: "./image/memory.jpg",
+      alt: "Memory Game Photo",
+      netlifyLink: "https://memory-cards-2.netlify.app/",
+      githubLink: "https://github.com/mayoush89k/memoryGame",
+      description:
+        "This is a teamwork, we built a game of shuffle cards, that the user must remember where he open the card so he chooses pair of same cards, until he finds them all",
+    },
+    {
+      id: 3,
+      name: "Ladder and Snake",
+      src: "./images/ladderAndSnake.jpg",
+      alt: "Ladder And Snake Photo",
+      netlifyLink: "https://ladder-and-snake.netlify.app",
+      githubLink: "https://github.com/mayoush89k/LadderAndSnake",
+      description:
+        "This is the game needs 4 players, each one has his turn of rolling dice, and move on the table of 100 steps, but when they stop on a bottom of ladder, they climb it to the top of it, but if they stop on a face of the snake, they drop down to its tail position, the game ends when one of them reach 100",
+    },
+    {
+      id: 4,
+      name: "Dice Game",
+      src: "./images/dice.jpg",
+      alt: "Dice Game Photo",
+      netlifyLink: "https://dice-23-game.netlify.app",
+      githubLink: "https://github.com/mayoush89k/DiceGame",
+      description:
+        "This game needed two players, that each one roll the dice until he holds his score, his score is counted as total of rolled dice numbers, but when he hold his score, he loses his turn. the game will finish until one of them get the target that they manage at the first",
+    },
+  ],
+
+  myWebPages: [
+    {
+      id: 1,
+      name: "Basketball Match",
+      src: "./images/basketball.jpg",
+      alt: "BasketBall Match Photo",
+      netlifyLink: "https://endearing-concha-13a7de.netlify.app",
+      githubLink: "https://github.com/fathi-zidan/js_group_project",
+      description:
+        "This is a teamwork, we built a basketball match, that has random events either goal or foul, until the game time has finished, then it shows the result with overview of the game",
+    },
+    {
+      id: 2,
+      name: "Color Palette",
+      src: "./images/colorPalette.jpg",
+      alt: "Color Palette Photo",
+      netlifyLink: "https://generate-color-palette.netlify.app",
+      githubLink: "https://github.com/mayoush89k/ColorPalette",
+      description:
+        "This is just generating a radom 5 colors, and show them to the screen. \nWe can save it to the history box, and can also copy the color code by clicking on the color itself",
+    },
+    {
+      id: 3,
+      name: "Flight Ticket",
+      src: "./images/flightTicket.jpg",
+      alt: "Flight Ticket Photo",
+      netlifyLink: "https://booking-flight-ticket.netlify.app/",
+      githubLink: "https://github.com/MayouSh89K/FlightTicket",
+      description:
+        "This is a website of booking a flight ticket. \n At the first there will be a login page, with checkbox if the user is regular or an admin. \n If the user is admin he can add more flights to the list, but the user only has the opportunity to look at the flights, sort them by price and search at them, also each flight has a button to add to cart, \n so the regular user can start booking by the first choose the suitable flight, and add it to his own cart. \n Then at the cart page he can book it with also opportunity adding number of travelers, and the system calculate the price and send him the confirmation alert",
+    },
+    {
+      id: 4,
+      name: "Diamond Painting Amazon Product",
+      src: "./images/diamondPainting.jpg",
+      alt: "Diamond Painting Amazon Product Photo",
+      netlifyLink: "https://diamond-painting-product.netlify.app",
+      githubLink: "https://github.com/mayoush89k/diamondPaintingProduct",
+      description:
+        "This is a web page of only HTML and CSS. only to learn how to build and design an existing page",
+    },
+    {
+      id: 5,
+      name: "Tesla S",
+      src: "./images/teslaS.jpg",
+      alt: "Tesla S Photo",
+      netlifyLink: "https://mayk-tesla-model-s.netlify.app",
+      githubLink: "https://github.com/mayoush89k/teslaWeb",
+      description:
+        "This is a web page of only HTML and CSS. only to learn how to build and design an existing page",
+    },
+    {
+      id: 6,
+      name: "World Wikipedia",
+      src: "./images/worldWikipedia.jpg",
+      alt: "World Wikipedia Photo",
+      netlifyLink: "https://mayoush89k.github.io/world-wikipedia/",
+      githubLink: "https://github.com/mayoush89k/world-wikipedia",
+      description:
+        "This a React website that brings all the details of each country in the world, from the wikipedia references. \n There are many pages, each page has another order list(\n1. list of the Countries' Names and Flags. \n2. list of Countries with list of each one's biggest cities. \n3. list of Countries with their capitals. \n4. list of Continents and Countries that belong to each one of them",
+    },
+  ],
+};
+
+const gamesListContainer = document.querySelector("#games-list");
+const websListContainer = document.querySelector("#webs-list");
+let rowSection = document.createElement("section");
+
+const gamesList = document.createElement("section");
+gamesList.className = "games-list"
+const websList = document.createElement("section");
+websList.className = "webs-list"
+
+displayGames(myWork.myGamesList);
+displayWebs(myWork.myWebPages);
+
+
+function displayGames(games) {
+  gamesList.innerText = "";
+  games.map((game, index) => {
+    if (index % 2 == 0) {
+      rowSection = document.createElement("section");
+      gamesList.append(rowSection);
+    }
+    const gameBox = document.createElement("section");
+    gameBox.classList.add("list-box");
+
+    // const img = document.createElement("img");
+    // img.setAttribute("url", "./images/bg.jpg");
+    // img.setAttribute("alt", game.alt);
+
+    const name = document.createElement("section");
+    name.innerText = game.name;
+    name.classList.add("name");
+
+    const netlify = document.createElement("a");
+    netlify.style.fontWeight = "bold";
+    netlify.href = game.netlifyLink;
+    netlify.innerText = "Go Live";
+
+    const newLine = document.createElement("br");
+    const github = document.createElement("a");
+    github.style.fontWeight = "bold";
+    github.href = game.githubLink;
+    github.innerText = "Code Link";
+
+    const desc = document.createElement("section");
+    desc.innerText = game.description;
+    desc.classList.add("description");
+
+    gameBox.append(name, netlify, newLine, github, desc);
+
+    rowSection.appendChild(gameBox);
+    gamesList.appendChild(rowSection);
+    gamesListContainer.appendChild(gamesList)
+  });
+}
+
+function displayWebs(webs) {
+  websList.innerText = "";
+  webs.map((web, index) => {
+    if (index % 2 == 0) {
+      rowSection = document.createElement("section");
+      websList.append(rowSection);
+    }
+    const webBox = document.createElement("section");
+    webBox.classList.add("list-box");
+
+    // const img = document.createElement("img");
+    // img.setAttribute("src", web.src);
+    // img.setAttribute("alt", web.alt);
+
+    const name = document.createElement("section");
+    name.innerText = web.name;
+    name.classList.add("name");
+
+    const netlify = document.createElement("a");
+    netlify.style.fontWeight = "bold";
+    netlify.href = web.netlifyLink;
+    netlify.innerText = "Go Live";
+
+    const newLine = document.createElement("br");
+    const github = document.createElement("a");
+    github.style.fontWeight = "bold";
+    github.href = web.githubLink;
+    github.innerText = "Code Link";
+
+    const desc = document.createElement("section");
+    desc.innerText = web.description;
+    desc.classList.add("description");
+
+    webBox.append(name, netlify, newLine, github, desc);
+
+    rowSection.appendChild(webBox);
+    websList.appendChild(rowSection);
+    websListContainer.appendChild(websList)
+});
+}
+
+const searchBtn = document.querySelector("#search");
+
+searchBtn.addEventListener("input", (event) => {
+  const text = event.target.value;
+  console.log(text);
+
+  displayGames([
+    ...myWork.myGamesList.filter((game) =>
+      game.name.toLowerCase().includes(text.toLowerCase())
+    ),
+  ]);
+  displayWebs([
+    ...myWork.myWebPages.filter((web) =>
+      web.name.toLowerCase().includes(text.toLowerCase())
+    ),
+  ]);
+});
